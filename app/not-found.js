c6d805fc4d2b98fb049b970f941a9b768826ee77
Zheero0 <reeco.liburd@outlook.com>
2025-06-16 12:30:22 +0100

    dunno2

--- app/not-found.js
+++ app/not-found.js
@@ -6,7 +6,7 @@ export default function NotFound() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-white text-center px-4">
       <h1 className="text-5xl font-bold text-black mb-4">404</h1>
       <p className="text-lg text-gray-600 mb-6">
-        Oops! The page you're looking for doesn’t exist.
+        Oops! The page you&#39;re looking for doesn&#39;t exist.
       </p>
       <Link
         href="/"
